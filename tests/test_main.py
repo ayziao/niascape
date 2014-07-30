3eee15c06a4c92f0941860df773bfb652ec7548a
ayziao <ayziao+git@gmail.com>
2014-07-30 15:39:47 +0900

    wsgiclient unittest add

--- tests/test_main.py
+++ tests/test_main.py
@@ -13,3 +13,27 @@ class TestMyapp(unittest.TestCase):
 	def test_main(self):
 		ref = niascape.run()
 		self.assertEqual(ref, 'main')
+
+
+	def test_wsgiclient_root(self):
+		ret = ['']
+		env = {'PATH_INFO':'/'}
+		def wsgi(status: str, header:list):
+			ret[0] = status
+		niascape.application(env,wsgi)
+
+		self.assertEqual(ret[0], '200 OK')
+
+	def test_wsgiclient_favicon(self):
+		ret = ['']
+		env = {'PATH_INFO':'/favicon.ico'}
+		def wsgi(status: str, header:list):
+			ret[0] = status
+		niascape.application(env,wsgi)
+
+		self.assertEqual(ret[0], '404 Not Found')
+
+
+
+
+
