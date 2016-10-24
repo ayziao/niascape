365377c0f0db85638ede19402379f601d00b7667
ayziao <ayziao+git@gmail.com>
2016-10-24 09:07:44 +0900

    試作 ルーティングリファクタリング

--- 試作/router.php
+++ 試作/router.php
@@ -1,12 +1,27 @@
 <?php 
+
 //ルーティング
+function routing(){
+	$path = substr($_SERVER["SCRIPT_NAME"],1);
 
-$path = substr($_SERVER["SCRIPT_NAME"],1);
+	if (is_kobetu($path)) { //個別ページ判定	
+		//個別ページ
+		return require('kobetu.php');
+	} else {
+		return require($path . '.php');
+	}
 
-if (preg_match('/^[0-9]{20}?/', $path)) {	//数字20文字だったら個別ページ
-	//個別ページ
-	return require('kobetu.php');
-} else {
-	return require($path . '.php');
 }
 
+//個別ページ判定
+function is_kobetu($path){
+	return preg_match('/^[0-9]{20}?/', $path); //数字20文字だったら個別ページ
+}
+
+//ユーザ指定
+
+
+
+
+
+return routing();
