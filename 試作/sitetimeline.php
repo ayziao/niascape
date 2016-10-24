d760f72717bdf928b0c9cf2d17fdf8f763059cd0
ayziao <ayziao+git@gmail.com>
2016-10-24 12:04:55 +0900

    試作 サイト(ユーザ)タイムライン

--- 試作/sitetimeline.php
+++ 試作/sitetimeline.php
@@ -1,3 +1,136 @@
-sitetimeline
 <?php 
-phpinfo();
+header('Content-Type: text/html; charset=UTF-8');
+
+//サイトタイムライン
+
+$ini_array = parse_ini_file("setting.ini");
+$location = $ini_array['sqlite_file'];
+$handle = new SQLite3($location); 
+
+$user = explode("/", substr($_SERVER["SCRIPT_NAME"],2))[0];
+
+$query = <<< EOM
+
+SELECT * FROM basedata
+WHERE user = '$user'
+AND tags NOT LIKE '% gyazo_posted %'
+ORDER BY identifier DESC LIMIT 200
+
+EOM;
+
+$results = $handle->query($query); 
+$raw = $results->fetchArray();
+
+//print('<pre>');
+//var_dump($query);
+
+$day = '';
+
+//TODO 時間区切りを入れる
+
+while ($row = $results->fetchArray()) {
+	$day2 = substr($row['datetime'], 0,10);
+	if($day != $day2){
+		if($day != ''){
+			$content .= '</div>';
+		}
+		$content .= '<h5><a href="./'. str_replace('-', '', $day2).'">'.$day2.'</a></h5><div class="lines">';
+		$day = $day2;
+	}
+	$content .= '<div class="line"><span class="time"><a href="./'.$row['identifier'].'">18:28:18</a></span>&thinsp;'.$row['body'].'</div>'."\n";
+}
+$content .= '</div>';
+
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width">
+		<title>タイムライン <?=$user?></title>
+		<link rel="stylesheet" type="text/css" href="./common.css">
+		<link rel="stylesheet" type="text/css" href="./css.css">
+	</head>
+
+	<body>
+		<h1><?=$user?></h1>
+
+		<form action="./" method="POST" enctype="multipart/form-data" onsubmit="return submit();">
+			<div class="textarea">
+				<textarea id="box" name="body" onKeyup="showmojilen();"></textarea>
+			</div>
+			tag<input id="tag" class="text" type="text" name="tags" onKeyup="showmojilen();">
+			<input id="btn" class="submitbutton" type="submit" name="submit" value="post" onclick="return submit();">
+			<input class="file" type="file" name="file" accept="image/*">
+			<input type="hidden" name="user" value="test">ｓ
+			
+			<script type="text/javascript">
+				var key = "none";
+				var sbmit = false;
+				var textbox = document.getElementById('box');
+				var tag = document.getElementById('tag');
+				var submitButton = document.getElementById('btn');
+
+				submitButton.disabled = true;
+
+				textbox.addEventListener('keydown',
+						function (e) {
+							key = e.which;
+							if (sbmit === false && e.metaKey && e.which == 13) {
+								submitButton.click();
+								submit();
+							}
+						},
+						false
+						);
+
+				function showmojilen() {
+					var taglen = tag.value.trim().length;
+					if (taglen > 0) {
+						taglen += 2;
+					}
+					var bodylen = textbox.value.trim().length;
+					var strlen = bodylen + taglen;
+					if (sbmit === false){
+						if (bodylen === 0 || strlen > 140) {
+							submitButton.disabled = true;
+							submitButton.value = 'post';
+						} else {
+							submitButton.disabled = false;
+							submitButton.value = strlen;
+						}
+					}
+				}
+				
+				function submit(){
+					if(sbmit === true){ return false; };
+					
+					sbmit = true;
+					submitButton.disabled = true;
+					submitButton.value = '送信中';
+					
+					alert('ボタン、無効になってるでしょ？');
+					return true;
+				}
+
+				setInterval("showmojilen()", 1000);
+
+			</script>
+
+		</form>
+
+		<div id="etc"></div>
+		
+		<div>
+
+			<?=$content?>
+
+		</div>
+
+		<div><a href="./">top</a></div>
+		<form action="./" method="GET">
+			<input id="search" class="text" type="text" name="searchbody">
+			<input id="btn" class="submitbutton" type="submit" name="submit" value="検索">
+		</form>
+	</body>
+</html>
