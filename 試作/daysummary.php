6156978aa45b6edc81adf16ee83cfb4df22434c4
ayziao <ayziao+git@gmail.com>
2016-10-30 18:18:21 +0900

    修正 試作 CSSパス

--- 試作/daysummary.php
+++ 試作/daysummary.php
@@ -61,8 +61,8 @@ $content .= "\n\t\t\t</div>";
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width">
 		<title><?=$path?> <?=$user?></title>
-		<link rel="stylesheet" type="text/css" href="/common.css">
-		<link rel="stylesheet" type="text/css" href="/css.css">
+		<link rel="stylesheet" type="text/css" href="./common.css">
+		<link rel="stylesheet" type="text/css" href="./css.css">
 	</head>
 
 	<body>
