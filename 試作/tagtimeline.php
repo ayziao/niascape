c086b8d2e8079d63c52ae30c18262215eb520872
ayziao <ayziao+git@gmail.com>
2016-11-15 00:00:06 +0900

    修正 試作 favicon

--- 試作/tagtimeline.php
+++ 試作/tagtimeline.php
@@ -65,6 +65,7 @@ $content .= "\n\t\t\t</div>";
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width">
 		<title><?=$tag?> <?=$user?></title>
+		<link rel="icon" type="image/png" href="./favicon.png">
 		<link rel="stylesheet" type="text/css" href="/common.css">
 		<link rel="stylesheet" type="text/css" href="./css.css">
 	</head>
