552d88036ed78fea0fd705a2ab191f15ee94c91a
ayziao <ayziao+git@gmail.com>
2016-12-01 18:28:34 +0900

    試作 DBの\r\n検索テスト データ自体はエクスポートしてファイルの改行コード揃えてインポートした

--- 試作/test.php
+++ 試作/test.php
@@ -1,6 +1,45 @@
 <?php 
 date_default_timezone_set('Asia/Tokyo');
+//phpinfo();
 
-phpinfo();
+// \r\n検索
+
+$ini_array = parse_ini_file("setting.ini");
+$handle = new SQLite3($ini_array['sqlite_file2']); 
+
+$search = "\r\n";
+
+$query = <<< EOM
+SELECT * FROM basedata
+WHERE body LIKE '%$search%' 
+ORDER BY identifier ASC LIMIT 1000
+EOM;
+var_dump($query);
+
+$results = $handle->query($query); 
+
+while ($row = $results->fetchArray(SQLITE3_ASSOC)) {
+	var_dump($row);
+}
+
+$query = <<< EOM
+SELECT count(*) FROM basedata
+WHERE body LIKE '%$search%' 
+EOM;
+var_dump($query);
+
+$results = $handle->query($query); 
+while ($row = $results->fetchArray(SQLITE3_ASSOC)) {
+	var_dump($row);
+}
 
 
+$query = <<< EOM
+SELECT count(*) FROM basedata
+EOM;
+var_dump($query);
+
+$results = $handle->query($query); 
+while ($row = $results->fetchArray(SQLITE3_ASSOC)) {
+	var_dump($row);
+}
