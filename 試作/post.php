425dc2db9abda102f1afb5de3948019d2ebd12e7
ayziao <ayziao+git@gmail.com>
2016-12-18 01:56:40 +0900

    コード整理

--- 試作/post.php
+++ 試作/post.php
@@ -1,97 +1,81 @@
 <?php 
+//投稿
+
 $now = \DateTime::createFromFormat('U.u', sprintf('%6F', microtime(true)));
 $now->setTimezone( new DateTimeZone('Asia/Tokyo'));
 
 require "twitteroauth/autoload.php";
 use Abraham\TwitterOAuth\TwitterOAuth;
 
-header('Content-Type: text/html; charset=UTF-8');
+$ini_array = loadIni();
+$handle = new SQLite3($ini_array['sqlite_file']); 
 
-//投稿
-function post($now){
+$site = $_POST['site'];
+$body = str_replace("\r\n", "\n", trim($_POST['body']));
+$tags = trim($_POST['tags']);
 
-	$ini_array = loadIni();
-	$handle = new SQLite3($ini_array['sqlite_file']); 
+$tagstring = '';
+$filename = '';
+$gyazourl = '';
 
-	$site = $_POST['site'];
-	$body = str_replace("\r\n", "\n", trim($_POST['body']));
-	$tags = trim($_POST['tags']);
+if (strpos($_FILES['file']['type'],'image') !== false){ //画像投稿
+	$extension = strtolower(array_pop(explode('.', $_FILES['file']['name'])));
 
-	$tagstring = '';
-	$filename = '';
-	$gyazourl = '';
+	if (in_array($extension, ['jpg','jpeg','gif','png'])){ //添付ファイルが画像なら
 
-	$image_extension_arr = ['jpg','jpeg','gif','png'];
+		$gyazoresults = gyazopost($ini_array['gyazo'],$_FILES['file']['tmp_name']); //gyazo
 
-	if (strpos($_FILES['file']['type'],'image') !== false){ //画像投稿
-		$extension = strtolower(array_pop(explode('.', $_FILES['file']['name'])));
+		$gyazourl = json_decode($gyazoresults)->permalink_url;
 
-		if (in_array($extension, $image_extension_arr)){
+		$datetime = $now->format('Y-m-d H:i:s');
+		$identifier = $now->format('YmdHisu');
 
-			$gyazoresults = gyazopost($ini_array['gyazo'],$_FILES['file']['tmp_name']); //gyazo
+		$results = dbinsert($handle,$site,$identifier,$datetime,$identifier,' gyazo_posted ',$gyazoresults); //gyazo投稿情報
 
-			$gyazourl = json_decode($gyazoresults)->permalink_url;
+		$now = \DateTime::createFromFormat('U.u', sprintf('%6F', microtime(true)));
+		$now->setTimezone( new DateTimeZone('Asia/Tokyo'));
+		$tagstring .= " with_image:$identifier";
 
-			$datetime = $now->format('Y-m-d H:i:s');
-			$identifier = $now->format('YmdHisu');
-
-			$results = dbinsert($handle,$site,$identifier,$datetime,$identifier,' gyazo_posted ',$gyazoresults); //gyazo投稿情報
-
-			$now = \DateTime::createFromFormat('U.u', sprintf('%6F', microtime(true)));
-			$now->setTimezone( new DateTimeZone('Asia/Tokyo'));
-			$tagstring .= " with_image:$identifier";
-
-			$filename = '/tmp/'.$identifier .'.'.$extension;
-			move_uploaded_file($_FILES['file']['tmp_name'], $filename);
-		}
+		$filename = '/tmp/'.$identifier .'.'.$extension;
+		move_uploaded_file($_FILES['file']['tmp_name'], $filename);
 	}
+}
 
-	$tagstringsite = '';
-	if (mb_strlen($tags)){
-		$tagarr = explode(' ', mb_ereg_replace('\s+', ' ', $tags));
-		$tagstringsite .= ' #'.implode(' #', $tagarr); 
-	}
+if (mb_strlen($tags)){
+	$tagarr = explode(' ', mb_ereg_replace('\s+', ' ', $tags));
+	$tagstring .= ' #'.implode(' #', $tagarr); 
+}
+$tagstring = ' twitter_posted' . $tagstring;
+if(strlen($tagstring) > 0){
+	$tagstring .= ' ';
+}
 
+if($body){
 	$datetime = $now->format('Y-m-d H:i:s');
 	$identifier = $now->format('YmdHisu');
-
-	$tagstring .= ' twitter_posted' . $tagstringsite;
-
-	if(strlen($tagstring) > 0){
-		$tagstring .= ' ';
-	}
-
-
-	if($body){
-		$results = dbinsert($handle,$site,$identifier,$datetime,$identifier,$tagstring,$body);
-	}
-
-	// print('<pre>');
-	// var_dump($_FILES);
-	// var_dump($filename);
-	// var_dump(json_decode($gyazoresults));
-	// var_dump($tags);
-	// var_dump($tagarr);
-	// var_dump($results);
-	// var_dump($identifier);
-	// var_dump($_POST);
-	// var_dump($queryg);
-	// var_dump($query);
-	// var_dump($_SERVER);
-
-	header('Location: http://' . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"]);
-
-	ob_end_flush();
-	ob_flush();
-	flush();
-
-	if($body){
-		$path = dirname(__FILE__);
-		exec("nohup php -c '' '$path/multipost.php' '$site' '$identifier' '$filename' '$gyazourl'  > /dev/null &");
-	}
-
-	return;
+	$results = dbinsert($handle,$site,$identifier,$datetime,$identifier,$tagstring,$body);
+	$path = dirname(__FILE__);
+	exec("nohup php -c '' '$path/multipost.php' '$site' '$identifier' '$filename' '$gyazourl'  > /dev/null &");
 }
+
+header('Location: http://' . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"]);
+
+// header('Content-Type: text/html; charset=UTF-8'); print('<pre>');
+// var_dump($_FILES);
+// var_dump($filename);
+// var_dump(json_decode($gyazoresults));
+// var_dump($tags);
+// var_dump($tagarr);
+// var_dump($results);
+// var_dump($identifier);
+// var_dump($_POST);
+// var_dump($queryg);
+// var_dump($query);
+// var_dump($_SERVER);
+
+
+
+
 
 //DB insert
 function dbinsert($handle,$site,$identifier,$datetime,$title,$tags,$body){
@@ -134,6 +118,3 @@ function gyazopost($token,$file){
 	}
 	return $results;
 }
-
-
-return post($now);
