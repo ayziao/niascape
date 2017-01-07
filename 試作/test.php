22a73af46cbeb02d66cacb0562e1fe1441653b8a
ayziao <ayziao+github@gmail.com>
2017-01-07 15:45:47 +0900

    ツイッタータイムラインストリーム保存したやつからふぁぼ集計

--- 試作/test.php
+++ 試作/test.php
@@ -15,6 +15,7 @@ if (!$file) {
 
 $count = ['friends' => 0, 'tw' => 0, 'rt' => 0, 'delete' => 0, 'scrub_geo' => 0, 'event' => '■■■■■■■■■■■■■■'];
 $count2 = 0;
+$fav = [];
 
 while ($line = fgets($file)) {
 	$aaa = json_decode($line, true);
@@ -30,6 +31,16 @@ while ($line = fgets($file)) {
 		$count['scrub_geo'] ++;
 	} elseif ($aaa['event']) {
 		$count[$aaa['event']] ++;
+		if($aaa['event'] = 'favorite'){
+			// if(!$fav[$aaa['source']['id']]){
+			// 	$fav[$aaa['source']['id']] = ['name' => $aaa['source']['screen_name'], 'count' => 0];
+			// }
+			// $fav[$aaa['source']['id']]['count']++;
+			$fav[$aaa['source']['screen_name']]++;
+		}
+		// if($count['favorite'] == 1){
+		// 	var_dump($aaa);
+		// }
 	} else {
 		var_dump($aaa);
 	}
@@ -43,4 +54,29 @@ fclose($file);
 
 echo "\n";
 var_dump($count);
+asort($fav);
+//var_dump($fav);
 
+foreach ($fav as $key => $value) {
+	$bbb .= '<a href="https://twitter.com/'.$key.'">'."$key</a> ($value)<br>\n"; 
+}
+
+ob_start();
+
+?>
+<html>
+	<head>
+		<title>ふぁぼられ</title>
+	</head>
+	<body>
+		<h1><?=$argv[1] ?></h1>
+		<?=$bbb ?>
+	</body>
+</html>
+<?php
+
+$dump = ob_get_contents();
+ob_end_clean();
+
+
+file_put_contents($argv[2].'/fav.html', $dump);
