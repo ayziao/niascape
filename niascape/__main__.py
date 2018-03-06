bba174d0d4ddc45d520f3eb2aae8b6696e37cb15
ayziao <ayziao+github@gmail.com>
2018-03-06 15:54:18 +0900

    日別投稿数

--- niascape/__main__.py
+++ niascape/__main__.py
@@ -3,17 +3,46 @@
 
 # TODO メインモジュールの説明書く
 """
+import psycopg2
 
 
 def run() -> str:
 	return 'main'
 
 
-if __name__ == '__main__': # pragma: no cover
-	import os 
+def _daycount():
+	from psycopg2.extras import DictCursor
+	from niascape import ini
+	con = ini['postgresql'].get('connect')
+
+	sql = """
+	SELECT
+		to_char(DATE("datetime"),'YYYY-MM-DD') as "Date" ,
+		COUNT(*) as "count"
+	FROM basedata
+	WHERE site = 'test'
+	GROUP BY DATE("datetime")
+	ORDER BY DATE("datetime")  DESC 
+	LIMIT 400
+	"""
+
+	with psycopg2.connect(con) as conn:
+		with conn.cursor(cursor_factory=DictCursor) as cur:
+			cur.execute(sql)
+			rows = cur.fetchall()
+
+			dict_result = []
+			for row in rows:
+				dict_result.append(dict(row))
+
+	return dict_result
+
+
+if __name__ == '__main__':  # pragma: no cover
+	import os
 	import sys
 
-	sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))) 	# PENDING 実行環境へパッケージとしてインストールすればsys.path.append必要なくなるくさいがどうするか
+	sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))  # PENDING 実行環境へパッケージとしてインストールすればsys.path.append必要なくなるくさいがどうするか
 
 	from niascape import cli
 
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,3 +1,4 @@
+import unittest
 from unittest import TestCase
 
 import niascape
@@ -5,11 +6,17 @@ import niascape
 
 class TestMyapp(TestCase):
 	def test_main(self):
-		# print(niascape.init_time)
 		ref = niascape.run()
-		self.assertEqual('main' ,ref)
+		self.assertEqual('main', ref)
 
 	def test_loadini(self):
 		ini = niascape._readini('config.ini.sample')
 		self.assertEqual(['postgresql'], ini.sections())
-	# self.assertEqual(ini.sections(), niascape.ini.sections())
+
+	# TODO データベースに接続してのテストについて考える
+	@unittest.skip("データベース関連のテストを保留")
+	def test_daycount(self):
+		ini = niascape._readini('config.ini.sample')
+		niascape.ini = ini
+		ref = niascape.__main__._daycount()
+		self.assertEqual({'Date': '2018-02-18', 'count': 2}, ref[0])
