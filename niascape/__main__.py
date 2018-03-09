06365899bd0aad87bdc71f5cad9836d6b8f05f9e
ayziao <ayziao+github@gmail.com>
2018-03-10 06:03:52 +0900

    リファクタリング:サブパッケージ model 作成 DBアクセス関連移動

--- niascape/__main__.py
+++ niascape/__main__.py
@@ -3,64 +3,25 @@
 
 # TODO メインモジュールの説明書く
 """
-import psycopg2
 import logging
+
+logger = logging.getLogger(__name__)
 
 
 def run() -> str:
 	return 'main'
 
 
-def _daycount(site='test', tag='', searchbody=''):
-	from psycopg2.extras import DictCursor
-	from niascape import ini
-	con = ini['postgresql'].get('connect')
-
-	tagwhere = ''
-	bodywhere = ''
-
-	# fixme プレースホルダ使う
-
-	if tag != '':
-		tagwhere = f"AND (tags like '% {tag} %' or tags like '% {tag}:%')"
-	elif searchbody != '':
-		bodywhere = f"AND body LIKE '%{searchbody}%'"
-
-	sql = f"""
-	SELECT
-		to_char(DATE("datetime"),'YYYY-MM-DD') as "Date" ,
-		COUNT(*)                               as "count"
-	FROM basedata
-	WHERE site = '{site}'
-		{tagwhere}
-		{bodywhere}
-	GROUP BY DATE("datetime")
-	ORDER BY DATE("datetime") DESC
-	LIMIT 400
-"""
-	logging.debug(sql)
-
-	with psycopg2.connect(con) as conn:
-		with conn.cursor(cursor_factory=DictCursor) as cur:
-			cur.execute(sql)
-			rows = cur.fetchall()
-
-			dict_result = []
-			for row in rows:
-				dict_result.append(dict(row))
-
-	return dict_result
-
-
 if __name__ == '__main__':  # pragma: no cover
-	logging.basicConfig(level=logging.DEBUG)  # PENDING リリースとデバッグ切り替えどうしようか logging.conf調べる
-	# print(_daycount('test', '#test'))
-
 	import os
 	import sys
 
 	sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))  # PENDING 実行環境へパッケージとしてインストールすればsys.path.append必要なくなるくさいがどうするか
 
 	from niascape import cli
+
+	logging.basicConfig(level=logging.DEBUG)  # PENDING リリースとデバッグ切り替えどうしようか logging.conf調べる
+	logger.debug(__file__)
+	# logger.debug(os.path.abspath(__file__))
 
 	print(cli.cli())
---
+++ niascape/model/basedata.py
@@ -0,0 +1,50 @@
+import logging
+import psycopg2
+
+logger = logging.getLogger(__name__)
+
+
+def _daycount(site='test', tag='', searchbody=''):
+	from psycopg2.extras import DictCursor
+	from niascape import ini
+	con = ini['postgresql'].get('connect')
+
+	tagwhere = ''
+	bodywhere = ''
+
+	# fixme プレースホルダ使う
+
+	if tag != '':
+		tagwhere = f"AND (tags like '% {tag} %' or tags like '% {tag}:%')"
+	elif searchbody != '':
+		bodywhere = f"AND body LIKE '%{searchbody}%'"
+
+	sql = f"""
+	SELECT
+		to_char(DATE("datetime"),'YYYY-MM-DD') as "Date" ,
+		COUNT(*)                               as "count"
+	FROM basedata
+	WHERE site = '{site}'
+		{tagwhere}
+		{bodywhere}
+	GROUP BY DATE("datetime")
+	ORDER BY DATE("datetime") DESC
+	LIMIT 400
+"""
+	logger.debug(sql)
+
+	with psycopg2.connect(con) as conn:
+		with conn.cursor(cursor_factory=DictCursor) as cur:
+			cur.execute(sql)
+			rows = cur.fetchall()
+
+			dict_result = []
+			for row in rows:
+				dict_result.append(dict(row))
+
+	return dict_result
+
+
+if __name__ == '__main__':  # pragma: no cover
+	logging.basicConfig(level=logging.DEBUG)  # PENDING リリースとデバッグ切り替えどうしようか logging.conf調べる
+	print(_daycount('test', '#test'))
