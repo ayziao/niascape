8aa8a21217fb02c2402e9ec30eedd415d6f959b5
ayziao <ayziao+github@gmail.com>
2018-03-10 09:31:13 +0900

    ツール:カバレッジ計測結果HTML出力ブラウザ起動バッチ

---
+++ tests/covarage.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+PROJECT_DIR=$(cd $(dirname $0)/..;pwd)
+cd $PROJECT_DIR
+coverage run --branch --source=niascape tests
+coverage report -m 
+coverage html && open -a "Google Chrome" file://$PROJECT_DIR/htmlcov/index.html
