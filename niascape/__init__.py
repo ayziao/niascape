2cfacdba7a02f8423c4aa034bf78dddb5ad81fbe
ayziao <ayziao+github@gmail.com>
2018-03-11 06:22:39 +0900

    リファクタリング : IEDのコード補完でいらんメンバが見えるのがダサいなーって あと不要コメント削除

--- niascape/__init__.py
+++ niascape/__init__.py
@@ -1,39 +1,24 @@
 """
 niascape パッケージ
 
-パッケージ名/ 済み
-	パッケージ名/ 済み
-		メイン 済み
-		サブパッケージ/
-	セットアップ
-	りーどみー ファイルはある 中身を書く
-	ライセンス 済み
-
-	ドキュメント/ 必要になったら
-	ユニットテスト/ 済み
-		パッケージと同じディレクトリ構成
-		test_パッケージと同じファイル構成
-		全テスト ファイルはある 中身を書く
 """
 import os
-from datetime import datetime
-from configparser import ConfigParser
+import datetime
+import configparser
 
-from niascape import __main__, wsgiapplication
+from niascape.__main__ import run
+from niascape.wsgiapplication import application
 
-init_time = datetime.utcnow()
+init_time = datetime.datetime.utcnow()
 
 
 def _readini(file_name='config.ini'):
 	# PENDING フルパス受け付けるか検討
 	# PENDING config.ini が無い時 config.ini.sample を読むか検討
 	path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-	ini = ConfigParser()
+	ini = configparser.ConfigParser()
 	ini.read(path + '/' + file_name)
 	return ini
 
 
 ini = _readini()
-
-run = __main__.run
-application = wsgiapplication.application
