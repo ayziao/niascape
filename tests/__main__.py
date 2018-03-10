8f78e51fe52394c4bdc36484a38fcda2798a562b
ayziao <ayziao+github@gmail.com>
2018-03-10 11:37:38 +0900

    デバッグコード追加 : ロガーにもりもり出してみる カバレッジ計測時には出さずに直に実行したときだけもりもり出してみる

--- tests/__main__.py
+++ tests/__main__.py
@@ -6,18 +6,33 @@ testsディレクトリ内(サブディレクトリも)のtest*.pyを実行
 主に coverage 向け
 coverage run --branch --source=niascape tests
 """
+import logging
 import os
 import sys
 import unittest
+from pprint import pformat
+
+logger = logging.getLogger(__name__)
 
 if __name__ == "__main__":
-	# coverage 通すとディレクトリ実行時__file__がtests/__main__.pyにならない対策
-	if __file__ == "tests":
+	verbosity = 1
+	if __file__ == "tests":  # coverage 通すとディレクトリ実行時__file__がtests/__main__.pyにならない対策
 		path = os.path.abspath(__file__)
 	else:
 		path = os.path.dirname(os.path.abspath(__file__))
+		logging.basicConfig(level=logging.DEBUG)  # coverage通してない時デバッグ情報出す
+		if __file__ == "tests/__main__.py":
+			verbosity = 2
+
+	logger.debug("カレントディレクトリ:%s", os.getcwd())
+
+	logger.debug("実行中のスクリプトへの相対パス:%s", __file__)
+	logger.debug("実行中のスクリプトへの絶対パス:%s", os.path.abspath(__file__))
+	logger.debug("Python検索パス:\n%s", pformat(sys.path))
 
 	sys.path.append(os.path.dirname(path))
 
+	logger.debug("Python検索パス追加後:\n%s\n", pformat(sys.path))
+
 	all_tests = unittest.TestLoader().discover(path)
-	unittest.TextTestRunner(verbosity=2).run(all_tests)
+	unittest.TextTestRunner(verbosity=verbosity).run(all_tests)
