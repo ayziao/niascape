c2531afa905fe14a94deac64f7f1de1bb4054979
ayziao <ayziao+github@gmail.com>
2018-03-12 17:06:33 +0900

    ツール : IDEさんに直せいわれたの直した 変数の展開 変数名がどこまでなのか明示的に指定 へぇ [ci skip]

--- tests/covarage.sh
+++ tests/covarage.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 PROJECT_DIR=$(cd $(dirname $0)/..;pwd)
-cd $PROJECT_DIR
+cd ${PROJECT_DIR}
 rm -r htmlcov
 coverage run --branch --source=niascape tests
 coverage report -m 
-coverage html && open -a "Google Chrome" file://$PROJECT_DIR/htmlcov/index.html
+coverage html && open -a "Google Chrome" file://${PROJECT_DIR}/htmlcov/index.html
 cd htmlcov
 find . -type f -name "*.html" -print0 | xargs -0 sed -i -e "s/        /  /g"
