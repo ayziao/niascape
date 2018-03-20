d87bb1e64fced2aea44619b894d4f5700edf3edd
ayziao <ayziao+github@gmail.com>
2018-03-20 11:22:20 +0900

    機能追加 : コマンドライン引数パーサー

--- niascape/cli.py
+++ niascape/cli.py
@@ -13,7 +13,7 @@ logger = logging.getLogger(__name__)
 def cli():
 	# TODO コマンドライン引数を解決してニアスケイプRUNを実行して結果をよしなに出力
 	logger.debug("コマンドライン引数: %s", sys.argv)
-
+	
 	if len(sys.argv) > 1:
 		action = sys.argv[1]
 	else:
@@ -22,14 +22,62 @@ def cli():
 
 	# PENDING アクションなし例外きたらエラーコード終了？
 	import niascape
-	return niascape.run(action,sys.argv[2:])
+	return niascape.run(action, sys.argv[2:])
+
+
+def parse(argv: list):
+	arguments = []
+	option_dict = {}
+	short_options = []
+
+	option_name = ''
+
+	for argument in argv:
+		if argument[0:2] == '--':
+			if option_name != '':
+				option_dict[option_name] = True
+				option_name = ''
+			if '=' in argument:
+				c = argument[2:].split('=')
+				option_dict[c[0]] = c[1]
+			else:
+				option_name = argument[2:]
+		# logger.debug(argument + ': Long option')
+		elif argument[0] == '-':
+			logger.debug(argument + ': short option')
+			short_options.append(argument[1:])
+		else:
+			# logger.debug(argument + ': operand')
+			if option_name == '':
+				arguments.append(argument)
+			else:
+				option_dict[option_name] = argument
+				option_name = ''
+
+	if option_name != '':
+		option_dict[option_name] = True
+
+	return [arguments, option_dict, short_options]
 
 
 if __name__ == '__main__':  # pragma: no cover
 	sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))  # PENDING 実行環境へパッケージとしてインストールすればsys.path.append必要なくなるくさいがどうするか
 
 	logging.basicConfig(level=logging.DEBUG)  # PENDING リリースとデバッグ切り替えどうしようか logging.conf調べる
+	# logging.basicConfig(format='\033[0;31m%(asctime)s %(name)s\n[%(levelname)s] %(message)s\033[0m', level=logging.DEBUG)
 
 	# sys.argv.extend("daycount test #test test".split())
+	# sys.argv.extend("action test #test test".split())
 
-	print(cli())
+	# print(cli())
+
+	print(sys.argv)
+
+	print(parse(sys.argv))
+
+
+
+	parsed = parse(sys.argv)
+	arguments = parsed[0]
+	option_dict = parsed[1]
+	short_options = parsed[2]
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -3,7 +3,7 @@ from unittest import TestCase
 import sys
 
 import niascape
-
+from niascape.cli import parse
 
 class TestCli(TestCase):
 	def test_cli(self):
@@ -13,3 +13,13 @@ class TestCli(TestCase):
 		sys.argv.append('hoge')
 		ret = niascape.cli()
 		self.assertEqual('No Action', ret)
+
+	def test_parse(self):
+		argv = ['script','sub_command', 'path', '--option1', '--option2', 'hoge', '--option3=piyo', '-als', 'gfgfg' ,'-u']
+		ret = parse(argv)
+		
+		self.assertEqual([['script', 'sub_command', 'path', 'gfgfg'], {'option1': True, 'option2': 'hoge', 'option3': 'piyo'}, ['als','u']],ret)
+		
+		# ret = parse(sys.argv)
+		# self.assertEqual(sys.argv,ret)
+
