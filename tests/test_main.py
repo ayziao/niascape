59efdc8e18d2ba74c47d8ff50fe8a521461a9c93
ayziao <ayziao+github@gmail.com>
2018-04-07 12:04:32 +0900

    テスト : ログのテスト

--- tests/test_main.py
+++ tests/test_main.py
@@ -11,14 +11,17 @@ class TestMyPackage(TestCase):
 		ref = niascape.run('top')
 		self.assertEqual('top', ref)
 
-		ref = niascape.run('hoge')
-		self.assertEqual('No Action', ref)
-
 		# モジュール変数をうっかり呼ばないか
 		ref = niascape.run('basedata')
 		self.assertEqual('No Action', ref)
 		ref = niascape.run('json')
 		self.assertEqual('No Action', ref)
+
+	def test_run_no_action(self):
+		with self.assertLogs('niascape', level='INFO') as cm:
+			ref = niascape.run('hoge')
+			self.assertEqual('No Action', ref)
+		self.assertEqual(cm.output, ['INFO:niascape:アクションなし: hoge'])
 
 	def test_read_ini(self):
 		ini = niascape._read_ini('config.ini.sample')
