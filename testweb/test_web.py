1f5eacaae2c04a880ae44df6f2e5658e55af89cd
ayziao <ayziao+github@gmail.com>
2019-06-01 17:28:39 +0900

    テスト : nsweb DBテスト追加

--- testweb/test_web.py
+++ testweb/test_web.py
@@ -3,7 +3,7 @@ from nsweb import create_app
 
 def test_config():
 	assert not create_app().testing
-	# assert create_app({'TESTING': True}).testing
+	assert create_app({'TESTING': True}).testing
 
 def test_hello(client):
 	response = client.get('/hello')
