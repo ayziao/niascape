7fd3a8f355440fadb91c7eb3ef841346c050010f
ayziao <ayziao+github@gmail.com>
2019-06-04 14:55:34 +0900

    機能追加 : nsweb task 更新

--- nsweb/task.py
+++ nsweb/task.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, redirect, flash, url_for
+from flask import Blueprint, render_template, request, redirect, flash, url_for, abort
 from nsweb.db import get_db
 
 bp = Blueprint('task', __name__, url_prefix='/task')
@@ -46,3 +46,51 @@ def create():
 			return redirect(url_for('task.index'))
 
 	return render_template('task/create.html', defaultowner=defaultowner, defaulttag=defaulttag)
+
+
+def get_task(id, check_author=True):
+	db = get_db()
+
+	task = db.execute(
+		'SELECT *'
+		' FROM task'
+		' WHERE "連番" = ?',
+		(id,)
+	).fetchone()
+
+	if task:
+		return task
+
+	abort(404, "task id {0} doesn't exist.".format(id))
+	return task
+
+
+@bp.route('/<int:id>/update', methods=('GET', 'POST'))
+# @login_required
+def update(id):
+	task = get_task(id)
+
+	if request.method == 'POST':
+		owner = request.form['owner']
+		title = request.form['title']
+		tag = ' ' + request.form['tag'].strip() + ' '
+		body = request.form['body']
+
+		error = None
+
+		if not title:
+			error = 'Title is required.'
+
+		if error is not None:
+			flash(error)
+		else:
+			db = get_db()
+			db.execute(
+				'UPDATE task SET "所有者" = ?, "タスク名" = ?, "タグ" = ?, "備考" = ?, "変更日時" = datetime("now", "utc")'
+				' WHERE "連番" = ?',
+				(owner, title, tag, body, id)
+			)
+			db.commit()
+			return redirect(url_for('task.index'))
+
+	return render_template('task/update.html', task=task)
--- testweb/test_task.py
+++ testweb/test_task.py
@@ -1,15 +1,27 @@
 from nsweb.db import get_db
 
+
 def test_index(client):
 	response = client.get('/task')
 	assert 'タスク' in response.data.decode('utf-8')
 	assert '未' in response.data.decode('utf-8')
 
 
-def test_create(client,app):
+def test_create(client, app):
 	assert client.get('/task/create').status_code == 200
-	client.post('/task/create', data={'owner':'test','title': 'created', 'tag': '', 'body': ''})
+	client.post('/task/create', data={'owner': 'test', 'title': 'created', 'tag': '', 'body': ''})
 	with app.app_context():
 		db = get_db()
 		count = db.execute('SELECT COUNT("連番") FROM task').fetchone()[0]
 		assert count == 2
+
+
+def test_update(client, app):
+	# auth.login()
+	assert client.get('/task/1/update').status_code == 200
+	client.post('/task/1/update', data={'owner': 'test', 'title': 'updated', 'tag': '', 'body': ''})
+
+	with app.app_context():
+		db = get_db()
+		post = db.execute('SELECT * FROM task WHERE "連番" = 1').fetchone()
+		assert post['タスク名'] == 'updated'
