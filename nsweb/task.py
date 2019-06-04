8b4948f666fba2867cf92c67071962552eee669c
ayziao <ayziao+github@gmail.com>
2019-06-04 09:21:37 +0900

    機能追加 : nsweb task 新規投稿

--- nsweb/task.py
+++ nsweb/task.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, flash, url_for
 from nsweb.db import get_db
 
 bp = Blueprint('task', __name__, url_prefix='/task')
@@ -15,3 +15,34 @@ def index():
 	).fetchall()
 
 	return render_template('task/index.html', tasks=tasks)
+
+
+@bp.route('/create', methods=('GET', 'POST'))
+# @login_required
+def create():
+	defaultowner = '未定'
+	defaulttag = request.args.get('tag', '')
+
+	if request.method == 'POST':
+		owner = request.form['owner']
+		title = request.form['title']
+		tag = ' ' + request.form['tag'].strip() + ' '
+		body = request.form['body']
+		error = None
+
+		if not title:
+			error = 'Title is required.'
+
+		if error is not None:
+			flash(error)
+		else:
+			db = get_db()
+			db.execute(
+				'INSERT INTO task ("所有者", "タスク名", "タグ", "備考")'
+				' VALUES (?, ?, ?, ?)',
+				(owner, title, tag, body)
+			)
+			db.commit()
+			return redirect(url_for('task.index'))
+
+	return render_template('task/create.html', defaultowner=defaultowner, defaulttag=defaulttag)
--- testweb/test_task.py
+++ testweb/test_task.py
@@ -1,6 +1,15 @@
+from nsweb.db import get_db
 
 def test_index(client):
 	response = client.get('/task')
 	assert 'タスク' in response.data.decode('utf-8')
 	assert '未' in response.data.decode('utf-8')
 
+
+def test_create(client,app):
+	assert client.get('/task/create').status_code == 200
+	client.post('/task/create', data={'owner':'test','title': 'created', 'tag': '', 'body': ''})
+	with app.app_context():
+		db = get_db()
+		count = db.execute('SELECT COUNT("連番") FROM task').fetchone()[0]
+		assert count == 2
