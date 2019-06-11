a93071393eac0b5b2de05de3498484e470f1f74a
ayziao <ayziao+github@gmail.com>
2019-06-11 14:04:08 +0900

    仕様変更 : basedataのUTC日時の名前修正

--- niascape/entity/basedata.py
+++ niascape/entity/basedata.py
@@ -19,7 +19,7 @@ class Basedata:
 	# tags: str
 	# body: str
 	# datetime: datetime
-	# utctime: datetime
+	# utcdatetime: datetime
 
 	def __init__(self, identifier: str, title: str, tags: str, body: str, datetime_: datetime) -> None:
 		self.identifier = identifier
@@ -28,10 +28,10 @@ class Basedata:
 		self.body = body
 		if type(datetime_) is datetime:
 			self.datetime = datetime_
-			self.utctime = datetime_ - timedelta(hours=9)
 		else:
 			self.datetime = datetime.strptime(datetime_[0:19], '%Y-%m-%d %H:%M:%S')  # PENDING ミリ秒の塩梅をどうするか
-			self.utctime = self.datetime - timedelta(hours=9)
+
+		self.utcdatetime = self.datetime - timedelta(hours=9)
 
 		logger.log(5, self.__dict__)
 
